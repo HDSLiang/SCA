6bfec431c7e7b06871e50230c24f518ab156dfc1
HDSLiang <1173864758@qq.com>
2023-03-05 16:26:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sca/Config.java
@@ -0,0 +1,14 @@
+package com.example.sca;
+
+public class Config {
+
+    public static final String COS_SECRET_ID = "AKIDnqfGciZCaibEckcFeoiNo0xe6JI2WlkT";
+    public static final String COS_SECRET_KEY = "WmnY3OXR5TcV9XgZCajcGgrodUMEC5qU";
+    public static final String COS_APP_ID = "1302478155";
+
+    // 访问的endpoint地址
+    public static final String OSS_ENDPOINT = "http://oss-cn-shanghai.aliyuncs.com";
+    public static final String BUCKET_NAME = "请输入bucket名称";
+    public static final String OSS_ACCESS_KEY_ID = "*************";
+    public static final String OSS_ACCESS_KEY_SECRET = "*************";
+}
